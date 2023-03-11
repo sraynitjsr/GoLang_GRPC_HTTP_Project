d2975dcd8719676d96cbe6c03fee5496de43fb29
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-03-11 19:47:03 +0530

    Added add Design

--- design/design.go
+++ design/design.go
@@ -7,12 +7,12 @@ import (
 var _ = API("calc", func() {
 	Title("Calculator Service Front End")
 	Description("Service for multiplying numbers, a Goa teaser")
-    Server("calc", func() {
-        Host("localhost", func() {
-            URI("http://localhost:8000")
-            URI("grpc://localhost:8080")
-        })
-    })
+	Server("calc", func() {
+		Host("localhost", func() {
+			URI("http://localhost:8000")
+			URI("grpc://localhost:8080")
+		})
+	})
 })
 
 var _ = Service("calc", func() {
@@ -29,6 +29,29 @@ var _ = Service("calc", func() {
 
 		HTTP(func() {
 			GET("/multiply/{a}/{b}")
+		})
+
+		GRPC(func() {
+		})
+	})
+
+	Files("/openapi.json", "./gen/http/openapi.json")
+})
+
+var _ = Service("calc", func() {
+	Description("The calc service performs operations on numbers.")
+
+	Method("add", func() {
+		Payload(func() {
+			Field(1, "a", Int, "Left operand")
+			Field(2, "b", Int, "Right operand")
+			Required("a", "b")
+		})
+
+		Result(Int)
+
+		HTTP(func() {
+			GET("/add/{a}/{b}")
 		})
 
 		GRPC(func() {
