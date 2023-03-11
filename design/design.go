4d8e3f47569ca1b08f10b3b15eb5f88f990214f2
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-03-11 19:50:35 +0530

    Added divide Design

--- design/design.go
+++ design/design.go
@@ -64,6 +64,52 @@ var _ = Service("calc", func() {
 var _ = Service("calc", func() {
 	Description("The calc service performs operations on numbers.")
 
+	Method("add", func() {
+		Payload(func() {
+			Field(1, "a", Int, "Left operand")
+			Field(2, "b", Int, "Right operand")
+			Required("a", "b")
+		})
+
+		Result(Int)
+
+		HTTP(func() {
+			GET("/add/{a}/{b}")
+		})
+
+		GRPC(func() {
+		})
+	})
+
+	Files("/openapi.json", "./gen/http/openapi.json")
+})
+
+var _ = Service("calc", func() {
+	Description("The calc service performs operations on numbers.")
+
+	Method("divide", func() {
+		Payload(func() {
+			Field(1, "a", Int, "Left operand")
+			Field(2, "b", Int, "Right operand")
+			Required("a", "b")
+		})
+
+		Result(Int)
+
+		HTTP(func() {
+			GET("/divide/{a}/{b}")
+		})
+
+		GRPC(func() {
+		})
+	})
+
+	Files("/openapi.json", "./gen/http/openapi.json")
+})
+
+var _ = Service("calc", func() {
+	Description("The calc service performs operations on numbers.")
+
 	Method("multiply", func() {
 		Payload(func() {
 			Field(1, "a", Int, "Left operand")
